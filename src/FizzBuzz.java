c85e953b3eab650c1382e36d59d62f4f9a3c0674
NkemOhanenye <nohanenye3527@students.mc3.edu>
2017-01-20 14:35:57 -0500

    Initial commit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,16 @@
+/**
+ * Created by otakusenseihig on 1/11/17.
+ */
+public class FizzBuzz {
+    public static void main(String args[]){
+        for (int i=0;i<101;i++){ //had to use for loop to get in the numbers
+            if (i%5==0){ //had trouble deciding but thought using if would be faster to find the numbers //mods of 5
+                System.out.print("Fizz"); //print Fizz
+            }
+            if (i%7==0){ //gives the mods of 7
+                System.out.print("Buzz"); //print Buzz
+            }
+            System.out.println(i); //print i //couldnt figure out how to print before FizzBuzz
+        }
+    }
+}
